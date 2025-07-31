abeca3c113aa753177511a1363436cd558ce5e58
umme-Tahazzee <tahazzeekaria1@gmail.com>
2025-07-31 12:05:41 +0600

    login from

---
+++ src/app/AppComponents/singup/AppLogo.tsx
@@ -0,0 +1,32 @@
+import { FaCheckDouble } from "react-icons/fa6"
+
+
+const AppLogo = () => {
+
+  return (
+  <div className="flex gap-2 items-center mb-11 
+  justify-center " >
+   <div className="bg-green-800 
+            rounded-sm  p-2" >
+    <FaCheckDouble className="size-6 text-white " />
+   </div>
+
+
+   <div className="">
+    <h1 className="text-2xl font-semibold
+           ">
+     <span className="text-green-600">
+      Quick
+     </span>
+     {" "}
+     Task
+    </h1>
+
+
+   </div>
+  </div>
+ )
+
+}
+
+export default AppLogo
